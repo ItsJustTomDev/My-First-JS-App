c90f02149f6632fc4605f27290273589d96e5b1e
ItsJustTomDev <96871598+ItsJustTomDev@users.noreply.github.com>
2022-02-26 12:43:49 +0200

    Fixed Repository.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,23 @@
 let pokemonList = [];
 
-let pokemonIvysaur = {
-  name: "Ivysaur",
-  height: "1 m",
-  types: ["grass", "normal", "poison"],
-};
+let pokemonRepo = [
+  {
+    name: "Ivysaur",
+    height: "1 m",
+    types: ["grass", "normal", "poison"],
+  },
+  {
+    name: "Charmander",
+    height: "0.6 m",
+    types: ["steel", "fire", "ice", "pairy"],
+  },
+  {
+    name: "Charmeleon",
+    height: "1.1 m",
+    types: ["steel", "fire", "ice", "pairy"],
+  },
+];
 
-let pokemonCharmander = {
-  name: "Charmander",
-  height: "0.6 m",
-  types: ["steel", "fire", "ice", "pairy"],
-};
-
-let pokemonCharmeleon = {
-  name: "Charmeleon",
-  height: "1.1 m",
-  types: ["steel", "fire", "ice", "pairy"],
-};
-
-pokemonList.push(pokemonIvysaur);
-pokemonList.push(pokemonCharmander);
-pokemonList.push(pokemonCharmeleon);
+pokemonList.push(pokemonRepo);
 
 console.log(pokemonList);
