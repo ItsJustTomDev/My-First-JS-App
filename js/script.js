ee18e9e22e28b8edcbd2c8f5ca1222e0be8c49c3
ItsJustTomDev <96871598+ItsJustTomDev@users.noreply.github.com>
2022-02-26 12:41:17 +0200

    Added pokemon objects
    Added objects to array.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
-alert("Hello world");
+let pokemonList = [];
 
-let favoriteFood = "Pizza";
-document.write(favoriteFood);
+let pokemonIvysaur = {
+  name: "Ivysaur",
+  height: "1 m",
+  types: ["grass", "normal", "poison"],
+};
+
+let pokemonCharmander = {
+  name: "Charmander",
+  height: "0.6 m",
+  types: ["steel", "fire", "ice", "pairy"],
+};
+
+let pokemonCharmeleon = {
+  name: "Charmeleon",
+  height: "1.1 m",
+  types: ["steel", "fire", "ice", "pairy"],
+};
+
+pokemonList.push(pokemonIvysaur);
+pokemonList.push(pokemonCharmander);
+pokemonList.push(pokemonCharmeleon);
+
+console.log(pokemonList);
