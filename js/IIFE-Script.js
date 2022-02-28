b64d2dea5e4e7c1663b9fe2aa15349797076d04c
ItsJustTomDev <96871598+ItsJustTomDev@users.noreply.github.com>
2022-02-28 15:44:52 +0200

    Added buttons and list items

---
+++ js/IIFE-Script.js
@@ -0,0 +1,57 @@
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "dog",
+      height: 0.2,
+      strenght: "water",
+    },
+  ];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(item) {
+    pokemonList.push(item);
+  }
+
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listPokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listPokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
+
+    let detailButton = document.createElement("button");
+    detailButton.innerText = "Click for details";
+    pokemonList.appendChild(detailButton);
+
+    detailButton.addEventListener("click", function (event) {
+      showDetails(pokemon);
+    });
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+  };
+})();
+
+let addPokemon = pokemonRepository.add({
+  name: "cat",
+  height: 0.5,
+  strenght: "fire",
+});
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach((pokemon) => {
+  pokemonRepository.addListItem(pokemon);
+});
--- js/script.js
+++ js/script.js
@@ -1,4 +1,38 @@
-alert("Hello world");
+let pokemonList = []; // Create a empty array
 
-let favoriteFood = "Pizza";
-document.write(favoriteFood);
+// Make the pokemon repoistory with the values inside
+let pokemonRepo = [
+  {
+    name: "Ivysaur",
+    height: "1 m",
+    types: ["grass", "normal", "poison"],
+  },
+  {
+    name: "Charmander",
+    height: "0.6 m",
+    types: ["steel", "fire", "ice", "pairy"],
+  },
+  {
+    name: "Charmeleon",
+    height: "1.1 m",
+    types: ["steel", "fire", "ice", "pairy"],
+  },
+];
+
+// forEach loop to get them item by item pushed into the array
+pokemonRepo.forEach((pokemons) => {
+  pokemonList.push(pokemons);
+});
+
+// loop through all items
+pokemonList.forEach((pokemon) => {
+  if (pokemon.height == "0.6 m") {
+    document.write(
+      `${pokemon.name} height (${pokemon.height}) - Fire is cool! <br>` // IF the one of the pokemons heights is 0.6 add THEN this line executes
+    );
+  } else {
+    document.write(
+      `${pokemon.name} height (${pokemon.height}) <br>` // otherwise this get executed
+    );
+  }
+});
