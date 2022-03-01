774640e07214efacb07d3a366cf6e4f31f88aefd
ItsJustTomDev <96871598+ItsJustTomDev@users.noreply.github.com>
2022-03-01 13:45:07 +0200

    Made functionallity
    Updated and placed commands

--- js/IIFE-Script.js
+++ js/IIFE-Script.js
@@ -1,20 +1,18 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {
-      name: "dog",
-      height: 0.2,
-      strenght: "water",
-    },
-  ];
+  let pokemonList = []; // create empty array
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=20"; // api URL we are fetching from
 
+  // fetchs all pokemons that are in the array
   function getAll() {
     return pokemonList;
   }
 
+  // adds a new pokemon to the array
   function add(item) {
     pokemonList.push(item);
   }
 
+  // This function creates a new pokemon to display on the html
   function addListItem(pokemon) {
     let pokemonList = document.querySelector(".pokemon-list");
     let listPokemon = document.createElement("li");
@@ -34,25 +32,58 @@ let pokemonRepository = (function () {
     });
   }
 
-  function showDetails(pokemon) {
-    console.log(pokemon);
+  // This function is a promise, it grabs the pokemons from the api and puts them into the array we made.
+  async function loadList() {
+    try {
+      const response = await fetch(apiUrl);
+      const json = await response.json();
+      json.results.forEach((item) => {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url,
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    } catch (e) {
+      console.error(e);
+    }
   }
 
+  // This function fetches the details from the pokemon.
+  async function loadDetails(item) {
+    let url = item.detailsUrl;
+    try {
+      const response = await fetch(url);
+      const details = await response.json();
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    } catch (e) {
+      return console.error(e);
+    }
+  }
+
+  // This function makes the details show in the console
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(() => {
+      console.log(pokemon);
+    });
+  }
+
+  // Here we return all functions
   return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
   };
 })();
 
-pokemonRepository.add({
-  name: "cat",
-  height: 0.5,
-  strenght: "fire",
-});
 
-console.log(pokemonRepository.getAll());
-
-pokemonRepository.getAll().forEach((pokemon) => {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () { // first we load the list
+  pokemonRepository.getAll().forEach((pokemon) => { // then we grab all pokemons
+    pokemonRepository.addListItem(pokemon); // and put them all to the DOM
+  });
 });
